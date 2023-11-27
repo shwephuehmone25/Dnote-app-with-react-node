c3b552c0b6bcd73e6fd97a4c7239fee71777a10a
shwephuehmone25 <shwephue7889@gmail.com>
2023-11-27 16:39:39 +0630

    get all notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,26 +4,31 @@ import {
   EyeIcon,
 } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
+// import formatISO9075 from "date-fns/formatISO9075";
+import { format } from "date-fns";
 
-const Note = () => {
+const Note = ({ note }) => {
+  const { _id, title, content, createdAt } = note;
+
+  const formattedDate = format(new Date(createdAt), "yyyy-MMM-dd");
+
   return (
-    <div className="w-3/5 border-t-4 border-t-teal-600 shadow-lg p-3">
-      <h3 className="text-xl font-medium">
-        It happened on Medium: October roundup
-      </h3>
-      <p className="text-sm">
-        October’s most-read stories, most-highlighted sentences, and updates for
-        writers — In the three months I’ve worked at Medium, I’ve answered the
-        question “what’s your favorite part of your new job?” too
-      </p>
-      <div className="flex items-center justify-end gap-2">
-        <TrashIcon width={20} className="text-red-600" />
-        <Link to={"/edit/1"}>
-          <PencilSquareIcon width={20} className="text-green-600" />
-        </Link>
-        <Link to={"/note/1"}>
-        <EyeIcon width={20} className="text-blue-600" />
-        </Link>
+    <div className="w-2/5 border-t-4 border-t-teal-600 shadow-lg p-3">
+      <h3 className="text-xl font-medium">{title}</h3>
+      <p className="text-sm">{content.slice(0, 120)}</p>
+      <div className="flex items-center justify-between mt-2 border-t pt-2">
+        <p className="text-sm font-medium">
+        Published At {formattedDate}
+        </p>
+        <div className="flex items-center justify-end gap-2">
+          <TrashIcon width={20} className="text-red-600" />
+          <Link to={"/edit/1"}>
+            <PencilSquareIcon width={20} className="text-green-600" />
+          </Link>
+          <Link to={"/note/1"}>
+            <EyeIcon width={20} className="text-blue-600" />
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
 import Note from "../components/Note";
 import Plus from "../components/Plus";
 
 const Index = () => {
+  const [ notes, setNotes] = useState([]);
+  const getAllNotes = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API}/notes`);
+    //console.log(response);
+    const notes = await response.json();
+    setNotes(notes);
+    //console.log(notes);
+  };
+  useEffect((_) => {
+    getAllNotes();
+  }, []);
   return (
-    <section className="flex gap-4 px-10 mt-10 flex-wrap">
-      <Note />
-      <Note />
-      <Note />
-      <Note />
+    <section className="flex gap-6 px-10 mt-10 flex-wrap">
+      {
+        notes.map(note => (<Note key={note._id} note={note} />))
+      }
       <Plus />
     </section>
   );
