940aec0780caba5bec5fd286acf0694995ca9bf2
shwephuehmone25 <shwephue7889@gmail.com>
2023-11-27 17:29:29 +0630

    add react-loader-spinner

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import Note from "../components/Note";
 import Plus from "../components/Plus";
+import { Hearts } from "react-loader-spinner";
 
 const Index = () => {
-  const [ notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading ] = useState(false);
+
   const getAllNotes = async () => {
+    setLoading(true);
     const response = await fetch(`${import.meta.env.VITE_API}/notes`);
     //console.log(response);
     const notes = await response.json();
     setNotes(notes);
+    setLoading(false);
     //console.log(notes);
   };
   useEffect((_) => {
@@ -16,9 +21,25 @@ const Index = () => {
   }, []);
   return (
     <section className="flex gap-6 px-10 mt-10 flex-wrap">
-      {
-        notes.map(note => (<Note key={note._id} note={note} />))
-      }
+      {!loading && notes.length > 0 ? (
+        <>
+          {notes.map((note) => (
+            <Note key={note._id} note={note} />
+          ))}
+        </>
+      ) : (
+        <div className="flex justify-center items-center w-full">
+          <Hearts
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="hearts-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={loading}
+          />
+        </div>
+      )}
       <Plus />
     </section>
   );
