f8ee3281e5691f70ff81543451c2e2a8e875a56c
shwephuehmone25 <shwephue7889@gmail.com>
2023-12-13 13:24:34 +0630

    pagination added

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
 import Note from "../components/Note";
-import { Hearts } from "react-loader-spinner";
+import { ColorRing } from "react-loader-spinner";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Index = () => {
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-  const getAllNotes = async () => {
+  const getAllNotes = async (pageNum) => {
     setLoading(true);
-    const response = await fetch(`${import.meta.env.VITE_API}/notes`);
+    const response = await fetch(`${import.meta.env.VITE_API}/notes?page=${pageNum}`);
     //console.log(response);
-    const notes = await response.json();
+    const { notes, totalNotes, totalPages } = await response.json();
+    setTotalPages(totalPages);
     setNotes(notes);
     setLoading(false);
     //console.log(notes);
@@ -20,6 +23,19 @@ const Index = () => {
   useEffect((_) => {
     getAllNotes();
   }, []);
+
+  /**pagination*/
+  const handlePre = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   /**create custom alert*/
   const customAlert = (message) => {
@@ -47,19 +63,39 @@ const Index = () => {
               customAlert={customAlert}
             />
           ))}
+          <div className="w-full flex items-center justify-center gap-3">
+            {currentPage > 1 && (
+              <button
+                type="button"
+                className=" text-white font-medium bg-teal-600 px-3 py-1"
+                onClick={handlePre}
+              >
+                Prev Page
+              </button>
+            )}
+            {currentPage < totalPages && (
+              <button
+                type="button"
+                className=" text-white font-medium bg-teal-600 px-3 py-1"
+                onClick={handleNext}
+              >
+                Next Page
+              </button>
+            )}
+          </div>
         </>
       ) : (
         <div className="flex justify-center items-center w-full">
           {loading ? (
-            <Hearts
-              height="80"
-              width="80"
-              color="#4fa94d"
-              ariaLabel="hearts-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={loading}
-            />
+            <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          />
           ) : (
             <p>No posts available</p>
           )}
